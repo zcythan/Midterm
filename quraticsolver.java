0bbe694dfec7176624d699fdeb773ee86c417ad4
zcythan <122485798+zcythan@users.noreply.github.com>
2024-03-07 14:37:55 -0500

    repeating steps B and C

--- quraticsolver.java
+++ quraticsolver.java
@@ -3,9 +3,9 @@ public class quraticsolver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 4;
-		int b = 5;
-		int c = 6;
+		int a = 7;
+		int b = 8;
+		int c = 9;
 		if (b*b - 4*a*c <0)
 			System.out.println("This equation " + a + "x^2 + " + b +"x + " + c +" has no solution");
 		else
